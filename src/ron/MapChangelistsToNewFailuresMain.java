3f7423c272a30491864fc4ce9495d4b0812564b5
b0b0b0b <b0b0b0b@gmail.com>
2013-10-31 08:15:40 -0700

    refactor out dupe code

--- src/ron/MapChangelistsToNewFailuresMain.java
+++ src/ron/MapChangelistsToNewFailuresMain.java
@@ -22,6 +22,34 @@ public class MapChangelistsToNewFailuresMain {
 
     TestFailuresByRun test_failures_by_run_id = TestFailuresByRun.readTestFailures(test_failures_tsv);
 
+    Map<String, Map<String, Integer>> changelist_to_failures = get_new_failures_by_changelist(cls, runs,
+        test_failures_by_run_id);
+
+    try (BufferedWriter out = new BufferedWriter(new FileWriter("changelist_to_failures_doc.txt"));
+        ProgressTracker pt = new ProgressTracker(null, "write", -1, "documents", "words", "bytes")) {
+      for (Map.Entry<String, Map<String, Integer>> e_failure_count : changelist_to_failures.entrySet()) {
+        Map<String, Integer> failure_count = e_failure_count.getValue();
+        String src_file_name = e_failure_count.getKey();
+        if (src_file_name.contains("\t")) throw new RuntimeException();
+        out.write(src_file_name);
+        StringBuilder sb = new StringBuilder();
+        for (Map.Entry<String, Integer> e : failure_count.entrySet()) {
+          for (int i=0; i<e.getValue(); i++) {
+            sb.append('\t');
+            sb.append(e.getKey());
+            pt.advise(0, 1, 0);
+          }
+        }
+        String s = sb.toString();
+        out.write(s);
+        out.write('\n');
+        pt.advise(1, 0, s.length());
+      }
+    }
+  }
+
+  static Map<String, Map<String, Integer>> get_new_failures_by_changelist(ChangelistSourceFiles cls,
+      TsvParser runs, TestFailuresByRun test_failures_by_run_id) {
     // for each run, create pointer back to most recent full run that
     // finished and did not fail
     int ptr = -1;
@@ -120,27 +148,6 @@ public class MapChangelistsToNewFailuresMain {
     }
 
     System.err.println("skipped: " + skipped_changelists+" handled: " + changelists+", failures: " + testfailures);
-
-    try (BufferedWriter out = new BufferedWriter(new FileWriter("changelist_to_failures_doc.txt"));
-        ProgressTracker pt = new ProgressTracker(null, "write", -1, "documents", "words", "bytes")) {
-      for (Map.Entry<String, Map<String, Integer>> e_failure_count : changelist_to_failures.entrySet()) {
-        Map<String, Integer> failure_count = e_failure_count.getValue();
-        String src_file_name = e_failure_count.getKey();
-        if (src_file_name.contains("\t")) throw new RuntimeException();
-        out.write(src_file_name);
-        StringBuilder sb = new StringBuilder();
-        for (Map.Entry<String, Integer> e : failure_count.entrySet()) {
-          for (int i=0; i<e.getValue(); i++) {
-            sb.append('\t');
-            sb.append(e.getKey());
-            pt.advise(0, 1, 0);
-          }
-        }
-        String s = sb.toString();
-        out.write(s);
-        out.write('\n');
-        pt.advise(1, 0, s.length());
-      }
-    }
+    return changelist_to_failures;
   }
 }
